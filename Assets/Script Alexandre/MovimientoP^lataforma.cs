7dc1b6ba95fd09f943ae4cd203e8cfe6fad7a2a4
Alexandre Fernandez <alexandre.fernandez@enti.cat>
2025-03-24 14:22:22 +0100

    Torres "arregladas"

--- Assets/Script Alexandre/MovimientoP^lataforma.cs
+++ Assets/Script Alexandre/MovimientoP^lataforma.cs
@@ -6,13 +6,39 @@ public class MovimientoPlataforma : MonoBehaviour
     public float direccionX = 1f;
     public float limiteDesaparicion = 30f;
 
+    [Header("Fade")]
+    public float fadeInicio = 0f;
+    public float fadeFin = 3f;
+
+    private SpriteRenderer[] sprites;
+
+    void Start()
+    {
+        sprites = GetComponentsInChildren<SpriteRenderer>();
+        SetAlpha(0f); // Comienza invisible
+    }
+
     void Update()
     {
         transform.position += new Vector3(direccionX * velocidad * Time.deltaTime, 0f, 0f);
 
-        if (Mathf.Abs(transform.position.x) > limiteDesaparicion)
+        float distancia = Mathf.Abs(transform.position.x);
+        float alpha = Mathf.InverseLerp(fadeInicio, fadeFin, distancia);
+        SetAlpha(alpha);
+
+        if (distancia > limiteDesaparicion)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void SetAlpha(float alpha)
+    {
+        foreach (var sr in sprites)
+        {
+            Color c = sr.color;
+            c.a = Mathf.Clamp01(alpha);
+            sr.color = c;
         }
     }
 }
