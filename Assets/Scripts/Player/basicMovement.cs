b26d95aa08bc7a356e76d7c110904d33bab778de
aleixberzal <aleix.berzal@enti.cat>
2025-03-24 13:21:31 +0100

    basicMovement

---
+++ Assets/Scripts/Player/basicMovement.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    public GameObject render;
+
+    [Header("Movimiento")]
+
+    private float horizontalMovement = 0f;
+
+    /*public variables to edit from inspector*/
+
+    [SerializeField] public float movementSpeed = 10f;
+    [SerializeField] public float forceController = 30f;
+    [SerializeField] public float airForce = 2f;
+    [SerializeField] public bool touchingFloor = true;
+
+    /*To turn the sprite around*/
+    private bool lookingRight = true;
+    
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();   
+    }
+
+    private void Update()
+    {
+        /*It gets the player's input (1 to go right, -1 to go left)*/
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontalMovement != 0)
+        {
+            ApplyForce(horizontalMovement);
+        }else
+        {
+            BreakMovement();
+        }
+    }
+
+    private void ApplyForce(float direction)
+    {
+
+        float actualSpeed = rb2D.velocity.x;
+        float targetSpeed = direction * movementSpeed;
+        float speedDiff = targetSpeed - actualSpeed;
+
+        Vector2 force;
+ 
+        force = new Vector2(speedDiff * forceController, 0);
+ 
+        rb2D.AddForce(force, ForceMode2D.Force);
+
+
+    }
+
+    private void BreakMovement()
+    {
+        float horizontalSpeed = rb2D.velocity.x;
+
+        rb2D.velocity = new Vector2(0f, rb2D.velocity.y);
+    }
+
+    private void Turn()
+    {
+        lookingRight = !lookingRight;
+        Vector3 scale = render.transform.localScale;
+        scale.x *= -1;
+        render.transform.localScale = scale;
+    }
+}
---
+++ Assets/Scripts/Player/jumpController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    [Header("Fuerza del Salto")]
+    [SerializeField] private float fuerzaDeSalto = 8.5f;
+    public bool isJumping;
+    public bool grounded;
+    private basicMovement movement;
+    public float jumpReduction = 3f;
+    private float originalSpeed;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground")) 
+        {
+            grounded = true;
+            isJumping = false; 
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground")) 
+        {
+            grounded = false;
+        }
+    }
+
+    private void Start()
+    {
+        movement = GetComponent<basicMovement>(); 
+        rb2D = GetComponent<Rigidbody2D>();
+        originalSpeed = movement.movementSpeed;
+    }
+
+    private void Update()
+    {
+        if (!isJumping && grounded) 
+        {
+            Saltar();
+        }
+    }
+
+    private void Saltar()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if (grounded)
+            {
+                rb2D.velocity = new Vector2(rb2D.velocity.x, fuerzaDeSalto); 
+                isJumping = true; 
+                grounded = false;
+            }
+        }
+    }
+}
