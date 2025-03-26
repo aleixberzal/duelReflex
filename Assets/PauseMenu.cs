d8f1791b36408aba4c8ea0af47cfa5acba039281
Jan Blagojevic Sanchez <jan.blagojevic@enti.cat>
2025-03-26 10:38:36 +0100

    pause

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject PausePanel;
-
-    // Update is called once per frame
-    void Update()
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    private void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (GameIsPaused) 
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }    
     }
-    public void Pause() 
+    void Resume()
     {
-       
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
 }
