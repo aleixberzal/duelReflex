008e9113f09a23a99f1c5b66f00295ce0b346c96
Jan Blagojevic Sanchez <jan.blagojevic@enti.cat>
2025-03-26 09:50:47 +0100

    escales

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Pause() 
+    {
+       
+    }
+}
