19aabee8ab55dc9f6a7a546284abde18916e1f0a
Jan Blagojevic Sanchez <jan.blagojevic@enti.cat>
2025-03-26 08:57:58 +0100

    jump animation

--- Assets/Scripts/Player/movement/jumpController.cs
+++ Assets/Scripts/Player/movement/jumpController.cs
@@ -19,6 +19,7 @@ public class JumpController : MonoBehaviour
     public bool grounded;
     public bool isJumping;
     private float originalSpeed;
+    private Animator Animator;
 
 
 
@@ -28,6 +29,7 @@ public class JumpController : MonoBehaviour
         rb2D = GetComponent<Rigidbody2D>();
         audioManager = FindObjectOfType<AudioManager>();
         originalSpeed = movement.movementSpeed;
+        Animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -60,8 +62,13 @@ public class JumpController : MonoBehaviour
 
     void Saltar()
     {
+        
         if (Input.GetKeyDown(jumpTecla))
         {
+            if (Animator != null)
+            {
+                Animator.SetTrigger("Jump");
+            }
             rb2D.velocity = new Vector2(rb2D.velocity.x, fuerzaDeSalto);
             isJumping = true;
             grounded = false;
